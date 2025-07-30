4053b73748e3fc4c661d920d7f547b442cb8cca1
Yonatan2385 <yoni22338855@gmail.com>
2025-07-30 17:59:13 +0300

    Initialize React frontend using Vite and clean default boilerplate
    at this point, everything here was added or removed by Vite setup, despite main.jsx, which I commited changes in

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>AI-Assisted Patient Visit Summaries</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
